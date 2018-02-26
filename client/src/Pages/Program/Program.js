4c4f7aacd596773564fb4ed6c4a9300bfd49f86e
Joy Hauser <joy.hauser@hotmail.com>
2018-02-26 17:50:29 -0600

    Add the program page

---
+++ client/src/Pages/Program/Program.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './../../CSS/Page.css';
+
+class Program extends Component {
+  render() {
+    return (
+      <div className="Program">
+        This is page that contains the program for the conference.
+      </div>
+    );
+  }
+}
+
+export default Program;
--- client/src/routes.js
+++ client/src/routes.js
@@ -5,6 +5,7 @@ import Archive from './Pages/Archive/Archive';
 import CallForParticipation from './Pages/CallForParticipation/CallForParticipation';
 import Submission from './Pages/Submission/Submission';
 import Conference from './Pages/Conference/Conference';
+import Program from './Pages/Program/Program';
 import Keynotes from './Pages/Keynotes/Keynotes';
 import Competition from './Pages/Competition/Competition';
 import Logistics from './Pages/Logistics/Logistics';
@@ -19,6 +20,7 @@ export const Main = () => (
 			<Route exact path='/callforparticipation' component={CallForParticipation} />
 			<Route exact path='/callforparticipation/submission' component={Submission} />
 			<Route exact path='/conference' component={Conference} />
+			<Route exact path='/conference/program' component={Program} />
 			<Route exact path='/conference/keynotes' component={Keynotes} />
 			<Route exact path='/competition' component={Competition} />
 			<Route exact path='/logistics' component={Logistics} />
