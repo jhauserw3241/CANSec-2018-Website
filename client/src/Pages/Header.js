b2a5425e0b8810c0d33ae4f9023a9afb526da7e2
Joy Hauser <joy.hauser@hotmail.com>
2018-02-26 17:03:46 -0600

    Update header to have normal nav style

--- client/src/Pages/Header.js
+++ client/src/Pages/Header.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 import logo from './../Images/logo.svg';
 import './../CSS/Header.css';
 
 class Header extends Component {
   render() {
     return (
-      <div className="Header">
-        <header className="Header-header">
-          <img src={logo} className="Header-logo" alt="logo" />
-          <h1 className="Header-title">Welcome to React</h1>
-        </header>
-      </div>
+      <header className="Header">
+        <div className="Navbar">
+          <div className="Header-logo">
+            <img src={logo} className="Logo-img" alt="logo" />
+          </div>
+          <div className="nav-body">
+            <NavLink to="/home" className="nav-link">Home</NavLink>
+					</div>
+        </div>
+      </header>
     );
   }
 }
